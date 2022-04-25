a231136de82a3a916cc6bc20b21f23526a20b3e5
Ryan <ryan@geekful.dev>
2022-04-26 00:05:26 +0800

    Use dynamic data in InfoPanel

--- src/InfoPanel.js
+++ src/InfoPanel.js
@@ -8,24 +8,24 @@ export class InfoPanel extends Component {
       <div id='infoPanel'>
         <div className='container-info'>
           <div id='infoPicDiv'>
-            <img id='infoPic' src={require('./img/bulbasaur.webp')} alt='bulbasaur-pic'/>
+            <img id='infoPic' src={require('./img/bulbasaur.webp')} alt='bulbasaur-pic' />
           </div>
           <div>
-            <div id='infoName'>Bulbasaur</div>
-            <div id='infoKind'>Entrancing Pokemon</div>
-            <div id='infoType'>Grass | Poison</div>
+            <div id='infoName'>{this.props.currentPokemon.name}</div>
+            <div id='infoKind'>{this.props.currentPokemon.kind}</div>
+            <div id='infoType'>{this.props.currentPokemon.type.map(type => <span>{type}</span>)}</div>
             <div id='infoHeight'>
               <div>Height</div>
-              <div className='heightWeight'>5'1"</div>
+              <div className='heightWeight'>{this.props.currentPokemon.height} cm</div>
             </div>
             <div id='infoWeight'>
               <div>Height</div>
-              <div className='heightWeight'>10 lbs.</div>
+              <div className='heightWeight'>{this.props.currentPokemon.weight} g</div>
             </div>
           </div>
         </div>
         <div id='infoTrivia'>
-          Bulbasaur is the only POKEMON blah blah.
+          {this.props.currentPokemon.trivia}
         </div>
       </div>
     )
